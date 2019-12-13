c5a8329cdb8f0819df65d90dd3ffa6921c4dd676
Hasan Can Volaka <hasancanvolaka@gmail.com>
2019-12-13 16:32:09 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+var http = require('http');
+var express = require("express");
+var RED = require("node-red");
+
+// Create an Express app
+var app = express();
+
+// Add a simple route for static content served from 'public'
+app.use("/",express.static("public"));
+
+// Create a server
+var server = http.createServer(app);
+
+// Create the settings object - see default settings.js file for other options
+var settings = {
+    httpAdminRoot:"/red",
+    httpNodeRoot: "/api",
+    userDir:"/Users/volaka/.nodered/",
+    adminAuth: {
+      type: "credentials",
+      users: [
+          {
+              username: "admin",
+              password: "$2a$08$04FYg46T3QhkEwkLE/wRIucIJ7efuCIpjNq8WZK/P2e6g/hosGklu",
+              permissions: "*"
+          }
+      ]
+    },
+    logging: "debug",
+    functionGlobalContext: { }    // enables global context
+};
+
+// Initialise the runtime with a server and settings
+RED.init(server,settings);
+
+// Serve the editor UI from /red
+app.use(settings.httpAdminRoot,RED.httpAdmin);
+
+// Serve the http nodes UI from /api
+app.use(settings.httpNodeRoot,RED.httpNode);
+
+server.listen(8080);
+
+// Start the runtime
+RED.start();
